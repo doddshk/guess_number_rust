99bfdbfcd9a6837a96f9c31abd8fb06ca8cd5cad
cu-sp <down1995@tutanota.com>
2024-12-13 00:44:29 -0500

    first commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use rand::Rng;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number.");
+                continue;
+            }
+        };
+
+        if guess < secret_number {
+            println!("Too small!");
+        } else if guess > secret_number {
+            println!("Too big!");
+        } else {
+            println!("You win!");
+            break;
+        }
+    }
+}
+
